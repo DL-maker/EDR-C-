06b81d7cb8e1626c658353c8077e0fa92e3f7a61
dl <dl@dl.dl>
2026-03-18 01:04:17 +0100

    Ajout de my_hexdump

---
+++ my_hexdump.cpp
@@ -0,0 +1,43 @@
+#include <bitset>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+int hex_file(const std::string& file){
+    std::ifstream fichier(file.c_str());
+    char b1 = fichier.get();
+    char b2 = fichier.get();
+    char b3 = fichier.get();
+    char b4 = fichier.get();
+    int nomber = 0;
+    int address = 16;
+    std::cout << std::setfill('0') << std::setw(8) << nomber << ' ' << std::hex << std::setw(2) << +b1 << ' ' << +b2 << ' ' << +b3 << ' ' << +b4 << '\n';
+    while (fichier){
+        nomber += 16;
+        address += 16;
+        std::cout << std::setfill('0') << std::setw(8) << nomber << ' ' << std::hex << std::setw(2) << +b1 << ' ' << +b2 << ' ' << +b3 << ' ' << +b4 << '\n';
+        b1 = fichier.get();
+        b2 = fichier.get();
+        b3 = fichier.get();
+        b4 = fichier.get();
+    };
+};
+
+int fichier_existe(const std::string& file) {
+    std::ifstream fichier(file.c_str());
+    if (fichier.good()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    for (int i = 1; i < argc; i++) {
+        if (fichier_existe(argv[i])) {
+            hex_file(argv[i]);
+        }
+    }
+    return 0;
+}
